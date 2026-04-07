d349236e9c89baba6fc702403f47588b7aa285bb
Udalov Igor <i.udalov@rambler-co.ru>
2026-04-07 21:32:44 +0300

    install simplecov, setup ci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+SimpleCov.start "rails" do
+  minimum_coverage 80
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
